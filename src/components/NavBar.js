5ce7e452392d7dee6f47433bd6b2fb043ffc39c6
Lauren Hoock <laurenh8916@protonmail.com>
2022-06-05 03:10:18 -0600

    completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const linksMapped = links.map(el => <a href={"#"+el} key={el}>{el}</a>)
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    {linksMapped}
+  </nav>;
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,15 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  const projectsMapped = projects.map(el => {
+  return <ProjectItem name={el.name} about={el.about} technologies={el.technologies} key={el.id} />
+})
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projectsMapped}
+      </div>
     </div>
   );
 }
